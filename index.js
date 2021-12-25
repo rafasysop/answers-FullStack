a126e64a9efd7fa11b7c039c0075b36270133eb7
Rafael Moura <rafaelcostamoura@gmail.com>
2021-12-25 20:39:05 -0300

    Adicionando Variaveis

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render("index"))
+app.get('/', (req, res) => {
+  const nome = 'rafa'
+  const lang = 'Javascript'
+  res.render("index", {
+    nome,
+    lang
+  })
+})
 
 
 app.listen(5005,(err) => {
