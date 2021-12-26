2345d7e549fbd12c748185751edda0bbfab4b314
Rafael Moura <rafaelcostamoura@gmail.com>
2021-12-25 22:00:42 -0300

    change server

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const express = require('express')
 
 const app = express()
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.get('/:nome?/:lang?', (req, res) => {
-  const { nome } = req.params
-  const { lang } = req.params
-  res.render("index", {
-    nome: nome || 'Gulmine',
-    lang: lang || 'JavaScript'
-  })
+
+app.get('/', (req, res) => {
+  const resp = {
+    status: res.statusCode,
+    date: new Intl.DateTimeFormat('pt-BR', { dateStyle: 'full', timeStyle: 'long'}).format(new Date()),
+    message: 'Bem Vindo ao Servidor Rafael Moura',
+    site: `http://${req.headers.host}/site`,
+    api: `http://${req.headers.host}/api`,
+
+  }
+  res.status(200).json(resp)
+})
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ apiVersion: '1.0' })
+})
+
+app.get('/site', (req, res) => {
+  res.render('index')
 })
 
 
